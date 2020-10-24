8e8f038166dd2b9a7ec57939548f22cd02917e2f
Stefan Andersson <stefananderssonoxe@gmail.com>
2020-10-25 01:37:04 +0200

    created a .gitignore and testproject

---
+++ frontend-tests/cypress/integration/regression-test-suite.spec.js
@@ -0,0 +1,9 @@
+/// <reference types="cypress"/>
+
+//test suit
+describe('Regression test suite', function(){
+    //test case
+    it('Perform valid login', function(){
+        cy.visit('http://localhost:3000')
+    })
+})
