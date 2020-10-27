9cf549ce4eb5dd67c13df7a2d7ec3c07f6c2b2ea
Stefan Andersson <stefananderssonoxe@gmail.com>
2020-10-27 16:45:45 +0100

    added seperet page for usernam_password and URL

--- frontend-tests/cypress/integration/regression-page-objects.spec.js
+++ frontend-tests/cypress/integration/regression-page-objects.spec.js
@@ -2,17 +2,18 @@
 
 import * as indexFuncs from '../pages/indexPage'
 import * as dashBoardFuncs from '../pages/dashboardPage'
+import * as targets from '../targets/targets'
 //test suite
 describe('Test suite', function(){
 
     beforeEach(()=>{
-        cy.visit('http://localhost:3000')
+        cy.visit(targets.base_url)
         indexFuncs.chechTitleOfIndexPage(cy)
     })
 
 // test case
     it('Perform login and logout', function(){
-        indexFuncs.performValidLogin(cy, 'Admin', 'Ultimate6', 'Tester Hotel Overview')
+        indexFuncs.performValidLogin(cy, targets.username,targets.password, 'Tester Hotel Overview')
         dashBoardFuncs.performLogout(cy, 'Login')
     })
 })
---
+++ frontend-tests/cypress/targets/targets.js
@@ -0,0 +1,5 @@
+module.exports ={
+    base_url: 'http://localhost:3000',
+    username: 'Admin',
+    password: 'Ultimate6'
+}
